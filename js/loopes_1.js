97db2f50c6452e934ae9ded864167f0f62928079
karush254 <111882141+karush254@users.noreply.github.com>
2023-01-26 00:16:08 +0400

    Add files via upload

---
+++ js/loopes_1.js
@@ -0,0 +1,64 @@
+//task 3
+// let str = "";
+// for(let i = 0; i<10; i++){
+//     for(let j = i ; j<10; j++){
+//         str += " * ";
+//     }
+//     str += "\n";
+// }
+// console.log(str);
+
+
+//Task 1
+// let str = "";
+// for(let i = 0; i<10; i++){
+//     for(let j = 10 - i ; j<10; j++){
+//         str += " * ";
+//     }
+//     str += "\n";
+// }
+// console.log(str);
+
+//task2
+// let n = 10;
+// let str = "";
+// for (let i = 0; i <= n; i++) {
+//   for (let j = 0; j < n - i; j++) {
+//     str += "  ";
+//   }
+//   for (let k = 0; k < i; k++) {
+//     str += " *";
+//   }
+//   str += "\n";
+// }
+// console.log(str);
+
+
+//task 5
+// let n = 10;
+// let str = "";
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n - i; j++) {
+//     str += " ";
+//   }
+//   for (let k = 0; k < 2 * i - 1; k++) {
+//     str += "*";
+//   }
+//   str += "\n";
+// }
+// console.log(str);
+
+//task4
+// let n = 10;
+// str = ""
+//   for (let i = 1; i <= n - 1; i++) {
+//     for (let j = 0; j < i; j++) {
+//      str += "  "
+//     }
+//     for (let k = 0; k < n - i; k++) {
+//       str += " *"
+//     }
+//    str += "\n"
+//   }
+//   console.log(str);
+
